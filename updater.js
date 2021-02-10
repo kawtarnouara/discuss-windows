a99eaa4b9aa937746d91657d99cd887e5f450d32
kawtar <kawtar.nouara@gmail.com>
2021-02-10 11:26:54 +0100

    add about && auto update

--- updater.js
+++ updater.js
@@ -203,7 +203,7 @@ exports.getUpdateInfo = getUpdateInfo = (showNoUpdates)  => {
     var body = JSON.stringify({ platform: 'desktop', os: 'windows'});
     const request = net.request({
         method: 'POST',
-        url: 'https://api.sand.private-discuss.com/v1.0/release/get',
+        url: 'https://api-piman.private-discuss.com/v1.0/release/get' ,
         protocol: 'https:',
     });
     request.on('response', (response) => {
